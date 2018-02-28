aecd0867de357c802fe311f1c8d4e85fd392edfd
Sloan Holzman <sloan.holzman@gmail.com>
2018-02-28 13:11:59 -0500

    add devise users

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :characters, dependent: :destroy
+  has_many :houses, dependent: :destroy
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'houses#index'
 
-root to: 'houses#index'
-
-resources :houses do
-  resources :characters do
-    resources :tags
+  resources :houses do
+    resources :characters do
+      resources :tags
+    end
   end
-end
-get 'categories/:id' => 'categories#show', :as => :category_show
+  get 'categories/:id' => 'categories#show', :as => :category_show
 
 end
